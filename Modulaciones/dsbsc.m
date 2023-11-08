9eafce9e476c32e211f2c9e9df0f318f63e5f55b
Jesús Rojas <90580391+JesusRV001@users.noreply.github.com>
2023-11-08 12:05:18 -0600

    Cambios (adicion del nuevo audio)

--- Modulaciones/dsbsc.m
+++ Modulaciones/dsbsc.m
@@ -1,19 +1,20 @@
 %%%%%%%%%%%%%%%%%%%%%%% DSB-SC (Portadora Suprimida) %%%%%%%%%%%%%%%%%%%%%%
 
-%Cargar la señal de audio
-rootdirectory = 'C:\Users\jesus\OneDrive - Estudiantes ITCR\TEC\2023\II Semestre 2023\Comunicaciones Eléctricas I\Tarea\Modulacion';
-[m, Fs] = audioread(fullfile(rootdirectory, 'prueba.m4a'));
+%Cargar la señal de audio  (stereo)
+[ms, Fs] = audioread('AudioTarea.m4a');
 
+% Promediar los canales izquierdo y derecho para obtener un canal mono
+m = mean(ms, 2);
 m = m(:);
 
 % Parámetros
-t = (0:length(m)-1) / Fs;
+t = (0:length(m)-1) / (Fs);
 Am = max(abs(m)); % Amplitud máxima de la señal de audio
-fm = 120; %PARA ESTE AUDIO
+fm = 716; %PARA ESTE AUDIO
 
 %Parametros de la señal portadora
-fc = 1000;                  % Frecuencia de la señal portadora 1kHz
-Ac = 8;                     % Amplitud de la señal portadora
+fc = 1500;                  % Frecuencia de la señal portadora 1kHz
+Ac = 10;                     % Amplitud de la señal portadora
 c = Ac*cos(2*pi*fc*t');     % Señal portadora
 subplot(3, 2, 3);
 plot(t,c);
@@ -31,7 +32,7 @@ DEPc = (1/(Fs*Nc))*abs(dftc).^2;  % Densidad espectral de potencia de c
 subplot(3, 2, 4);
 plot(f02,10*log10(DEPc))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-80 50]);
+ylim([-60 40]);
 xlabel('f [Hz]');
 ylabel('|C(f)|^2 [dB]');
 title('Señal portadora en la frecuencia ');
@@ -51,16 +52,17 @@ dftm = fftshift(fft(m));
 f01 = (-Nm/2:Nm/2-1)*(Fs/Nm);      
 DEPm = (1/(Fs*Nm))*abs(dftm).^2;  
 
+
 subplot(3, 2, 2);
 plot(f01,10*log10(DEPm))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-60 20]);
+ylim([-65 20]);
 xlabel('f [Hz]');
 ylabel('|M(f)|^2 [dB]');
 title('Señal moduladora en la frecuencia ');
 grid on
 
-% Señal DSB-SC modulada en el tiempo
+% Representacion de la señal modulada en el tiempo (DSB-SC)
 dsb_sc_modulated = m .*c;       % s(t) = m(t) * c(t)
 subplot(3, 2, 5);
 plot(t, dsb_sc_modulated);
@@ -68,12 +70,12 @@ xlabel('Tiempo (s)');
 ylabel('Amplitud');
 title('Señal DSB-SC Modulada');
 
+% Representacion de la señal modulada en la frecuencia (DSB-SC)
 Ny = length(dsb_sc_modulated);                   
 dfty = fftshift(fft(dsb_sc_modulated));          
 f0 = (-Ny/2:Ny/2-1)*(Fs/Ny);      
 DEPy = (1/(Fs*Ny))*abs(dfty).^2;  
 
-% PSD de la señal DSB-SC modulada
 subplot(3, 2, 6);
 plot(f0,10*log10(DEPy))
 xlim([-1.5*fc 1.5*fc]);
@@ -87,23 +89,24 @@ grid on
 
 % Agregar ruido a las señales moduladas
 
-snr_low = 30;       % Relación señal/ruido (bajo)
+snr_low = 20;        % Relación señal/ruido (bajo)
 snr_medium = 15;     % Relación señal/ruido (medio)
-snr_high = 8;       % Relación señal/ruido (alto)
+snr_high = 5;        % Relación señal/ruido (alto)
 
 
-% DSB-SC con ruido
+% Adicion del ruido a la señal modulada DSB-SC
 dsb_sc_modulated_noisy_low = awgn(dsb_sc_modulated, snr_low);
 dsb_sc_modulated_noisy_medium = awgn(dsb_sc_modulated, snr_medium);
 dsb_sc_modulated_noisy_high = awgn(dsb_sc_modulated, snr_high);
 
-% Llamar a la función para aplicar el filtro pasa banda con Ruido bajo
+% Llamar a la función para aplicar el filtro pasa banda de demodulacion con Ruido bajo
 [filtered_signal_r1, DEP_r1, f0_r1] = aplicarFiltroPasaBanda(dsb_sc_modulated_noisy_low, Fs, fc, fm,c);
-% Llamar a la función para aplicar el filtro pasa banda con Ruido medio
+% Llamar a la función para aplicar el filtro pasa banda de demodulacion con Ruido medio
 [filtered_signal_r2, DEP_r2, f0_r2] = aplicarFiltroPasaBanda(dsb_sc_modulated_noisy_medium, Fs, fc, fm,c);
-% Llamar a la función para aplicar el filtro pasa banda con Ruido alto
+% Llamar a la función para aplicar el filtro pasa banda de demodulacion con Ruido alto
 [filtered_signal_r3, DEP_r3, f0_r3] = aplicarFiltroPasaBanda(dsb_sc_modulated_noisy_high, Fs, fc, fm,c);
 
+%Graficas de las señales moduladas con ruido
 figure(2);
 subplot(3,1,1)
 plot(t,dsb_sc_modulated_noisy_low);
@@ -132,11 +135,12 @@ title('Señal demodulada con ruido medio')
 subplot(3,2,5)
 plot(t,filtered_signal_r3)
 title('Señal demodulada con ruido alto')
+
 % Espectros
 subplot(3,2,2)
 plot(f0_r1,10*log10(DEP_r1))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-60 20]);
+ylim([-40 20]);
 xlabel('f [Hz]');
 ylabel('|R1(f)|^2 [dB]');
 title('PSD señal demodulada con ruido bajo')
@@ -145,7 +149,7 @@ grid on
 subplot(3,2,4)
 plot(f0_r2,10*log10(DEP_r2))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-60 20]);
+ylim([-40 20]);
 xlabel('f [Hz]');
 ylabel('|R2(f)|^2 [dB]');
 title('PSD señal demodulada con ruido medio')
@@ -154,7 +158,7 @@ grid on
 subplot(3,2,6)
 plot(f0_r3,10*log10(DEP_r3))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-60 20]);
+ylim([-40 20]);
 xlabel('f [Hz]');
 ylabel('|R3(f)|^2 [dB]');
 title('PSD señal demodulada con ruido alto')
@@ -168,28 +172,27 @@ grid on
 
 %%%%%%%%%%%%%%%%%%%%%%%%% Demodulacion sin ruido aplicado %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%demodulated_signal = dsb_sc_modulated .* c;  % Demodulación usando la misma portadora
 [filtered_signal, DEPz, f0z] = aplicarFiltroPasaBanda(dsb_sc_modulated, Fs, fc, fm,c);
 
 
-% Señal demodulada
+% Señal demodulada sin ruido
 figure(4);
 subplot(2,1,1)
 plot(t,filtered_signal)
 title('Señal demodulada sin ruido')
 grid on
 
-% DEP de la señal demodulada
+% Espectro de la señal demodulada sin ruido
 subplot(2,1,2)
 plot(f0z,10*log10(DEPz))
 xlim([-1.5*fc 1.5*fc]);
-ylim([-50 20]);
+ylim([-40 20]);
 xlabel('f [Hz]');ylabel('|Z(f)|^2 [dB]')
 title('PSD señal demodulada sin ruido')
 grid on
 
 %Reproducir el audio demodulado
-%soundsc(filtered_signal, Fs);
+soundsc(filtered_signal, Fs);
 
 
 %%%%%%%%%% Funcion para aplicar el filtro de demodulacion %%%%%%%%%%%%%%%%%
